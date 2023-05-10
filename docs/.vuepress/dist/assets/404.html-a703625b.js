32e96d4f0d25195f5008c56be8f509715ced2c68
CloudCrusader <markuswagner93@gmail.com>
2023-05-10 16:28:09 +0200

    started to develop new homepage

---
+++ docs/.vuepress/dist/assets/404.html-a703625b.js
@@ -0,0 +1,1 @@
+import{_ as e,n as t,p as _}from"./framework-6fc7adbc.js";const c={};function n(r,o){return t(),_("div")}const a=e(c,[["render",n],["__file","404.html.vue"]]);export{a as default};
---
+++ vuepress.config.js
@@ -0,0 +1,12 @@
+import { defineUserConfig } from 'vuepress'
+import mixTheme from 'vuepress-theme-mix'
+
+export default defineUserConfig({
+  lang: 'en-US',
+  title: 'This is my new rework of my homepage',
+  description: 'Enjoy!',
+  theme: mixTheme({
+    // configure the theme here
+  }),
+
+})
